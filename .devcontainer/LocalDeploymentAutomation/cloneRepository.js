623deccf0e6c35987c44e46f50903267650fc34a
NTPLSagarPatel <118171394+NTPLSagarPatel@users.noreply.github.com>
2024-02-29 17:21:57 +0530

    Update cloneRepository.js

--- .devcontainer/LocalDeploymentAutomation/cloneRepository.js
+++ .devcontainer/LocalDeploymentAutomation/cloneRepository.js
@@ -7,7 +7,7 @@ multiLineCI()
 async function multiLineCI() {
 
     const branch = 'main'
-    // const webApiRepoPath = "/workspaces/devContainer/webApi";
+    const webApiRepoPath = "/workspaces/devContainer/webApi";
     const platformApiRepoPath = "/workspaces/devContainer/platformApi";
 
     if (!fs.existsSync(platformApiRepoPath)) {
@@ -18,13 +18,13 @@ async function multiLineCI() {
         child_process.execSync(`cd ${platformApiRepoPath} && git pull origin ${branch} --rebase`);
     }
 
-    // if (!fs.existsSync(webApiRepoPath)) {
-    //     console.log(`Cloning into ${webApiRepoPath} started...`);
-    //     child_process.execSync(`git clone https://github.com/NTPLSagarPatel/webapi.git  ${webApiRepoPath}`);
-    // }else {
-    //     console.log(`Pulling changes into ${webApiRepoPath} started...`);
-    //     child_process.execSync(`cd ${webApiRepoPath} && git pull origin ${branch} --rebase`);
-    // }
+    if (!fs.existsSync(webApiRepoPath)) {
+         console.log(`Cloning into ${webApiRepoPath} started...`);
+         child_process.execSync(`git clone https://github.com/NTPLSagarPatel/webapi.git  ${webApiRepoPath}`);
+     }else {
+         console.log(`Pulling changes into ${webApiRepoPath} started...`);
+         child_process.execSync(`cd ${webApiRepoPath} && git pull origin ${branch} --rebase`);
+     }
 
     CLI.close()
 }
